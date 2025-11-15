8a49e419a0f3981fd77559422122457f12cbf9a9
Javi <belikemike3057@hotmail.com>
2025-11-15 12:54:01 +0100

    Adding scoreboard, lives...

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ class Player {
     this.x = this.game.width * 0.5 - this.width * 0.5;
     this.y = this.game.height - this.height;
     this.speed = 5;
+    this.lives = 3;
   }
 
   draw(context) {
@@ -86,8 +87,22 @@ class Enemy {
       if (!projectile.free && this.game.checkCollision(this, projectile)) {
         this.markedForDeletion = true;
         projectile.reset();
+        if (!this.game.gameOver) this.game.score++;
       }
     });
+
+    // check collision enemies-player
+    if (this.game.checkCollision(this, this.game.player)) {
+      this.markedForDeletion = true;
+      if (!this.game.gameOver && this.game.score > 0) this.game.score--;
+      this.game.player.lives--;
+      if (this.game.player.lives < 1) this.game.gameOver = true;
+    }
+    // lose condition
+    if (this.y + this.height > this.game.height) {
+      this.game.gameOver = true;
+      this.markedForDeletion = true;
+    }
   }
 }
 
@@ -101,6 +116,7 @@ class Wave {
     this.speedX = 3;
     this.speedY = 0;
     this.enemies = [];
+    this.nextWaveTrigger = false;
     this.create();
   }
 
@@ -152,6 +168,10 @@ class Game {
 
     this.waves = [];
     this.waves.push(new Wave(this));
+    this.waveCount = 1;
+
+    this.score = 0;
+    this.gameOver = false;
 
     window.addEventListener("keydown", (e) => {
       if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
@@ -167,6 +187,7 @@ class Game {
   // event listeners
 
   render(context) {
+    this.drawStatusText(context);
     this.player.draw(context);
     this.player.update();
     this.projectilesPool.forEach((projectile) => {
@@ -176,6 +197,11 @@ class Game {
 
     this.waves.forEach((wave) => {
       wave.render(context);
+      if (wave.enemies.length < 1 && !wave.nextWaveTrigger && !this.gameOver) {
+        this.newWave();
+        this.waveCount++;
+        wave.nextWaveTrigger = true;
+      }
     });
   }
 
@@ -201,6 +227,42 @@ class Game {
       a.y + a.height > b.y
     );
   }
+
+  drawStatusText(context) {
+    context.save();
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2;
+    context.shadowColor = "black";
+    context.fillText("Score: " + this.score, 20, 40);
+    context.fillText("wave: " + this.waveCount, 20, 80);
+    for (let i = 0; i < this.player.lives; i++) {
+      context.fillRect(20 + 10 * i, 100, 5, 20);
+    }
+    if (this.gameOver) {
+      context.textAlign = "center";
+      context.font = "100px Impact";
+      context.fillText("GAME OVER!", this.width * 0.5, this.height * 0.5);
+      context.font = "20px Impact";
+      context.fillText(
+        "Press R to restart!",
+        this.width * 0.5,
+        this.height * 0.5
+      );
+    }
+    context.restore();
+  }
+
+  newWave() {
+    if (
+      Math.random() < 0.5 &&
+      this.columns * this.enemySize < this.width * 0.8
+    ) {
+      this.columns++;
+    } else if (this.rows * this.enemySize < this.height * 0.6) {
+      this.rows++;
+    }
+    this.waves.push(new Wave(this));
+  }
 }
 
 window.addEventListener("load", function () {
@@ -211,6 +273,7 @@ window.addEventListener("load", function () {
   ctx.fillStyle = "white";
   ctx.strokeStyle = "white";
   ctx.lineWidth = 5;
+  ctx.font = "30px Impact";
 
   const game = new Game(canvas);
 
